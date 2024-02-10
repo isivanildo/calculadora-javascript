4586bd0098c32ad8607c615e5171278e67b3db35
Ivanildo Ferreira <isivanildo@hotmail.com>
2024-02-10 11:57:08 -0300

    Trabalhando a exibição da data e hora no display da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,60 @@
 class CalcController {
-    
+
+    constructor() {
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currenteDate;
+        this.initialize();
+    }
+
+    initialize() {
+        this.setDisplayDatetime();
+
+        setInterval(() => {
+            this.setDisplayDatetime();
+        }, 1000)
+    }
+
+    setDisplayDatetime() {
+        this.displayDate = this.currenteDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
+        this.displayTime = this.currenteDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currenteDate() {
+        return new Date();
+    }
+
+    set currenteDate(date) {
+        this._currenteDate = date;
+    }
 }
